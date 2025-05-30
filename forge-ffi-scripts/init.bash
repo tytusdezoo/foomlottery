a576407c345fc14807884df6cf5ebef0ce2e0e16
Tytus de Zoo <leszek@example.com>
2025-05-30 12:50:41 +0000

    init logs.csv

--- forge-ffi-scripts/init.bash
+++ forge-ffi-scripts/init.bash
@@ -14,3 +14,4 @@ cp /dev/null www/00/00/index.csv
 echo '1,0,25439a05239667bccd12fc3bd280a29a02728ed44410446cbd51a27cda333b00,24d599883f039a5cb553f9ec0e5998d58d8816e823bd556164f72aef0ef7d9c0' > www/last.csv
 # betIndex,leafHash,betHash,betRand
 echo '0,24d599883f039a5cb553f9ec0e5998d58d8816e823bd556164f72aef0ef7d9c0,ce413930404e34f411b5117deff2a1a062c27b1dba271e133a9ffe91eeae520,0' > www/00/00/00.csv
+echo '0' > www/logs.csv
