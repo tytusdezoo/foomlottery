e1fd78244fc6122520c588182d7cd1ff36b1dc43
tytusdezoo <leszek@bioinfo.pl>
2025-06-05 17:53:34 +0200

    fix prayer

--- forge-ffi-scripts/monitor.js
+++ forge-ffi-scripts/monitor.js
@@ -172,8 +172,8 @@ async function readLogs(provider,lottery,generator,walletAddress) {
       }
       else if(log.event == "LogPrayer") {
         console.log("Prayer:", log.args);
-        // convert prayer bytes to string
-        const prayer = ethers.utils.toUtf8String(log.args.prayer);
+        // convert prayer = array of bytes32 values to string and trim 00 suffix
+        const prayer = log.args.prayer.map(p => ethers.utils.toUtf8String(p).replace(/\0*$/, '')).join("\n");
         writePrayer(log.args.betId,prayer);
       }
       else {
