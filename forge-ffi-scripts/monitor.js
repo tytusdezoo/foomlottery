28a581c9b3d9e94405eb45516878d5dd6c6a8e40
tytusdezoo <leszek@bioinfo.pl>
2025-06-05 11:40:46 +0200

    minBetSum in monitor

--- forge-ffi-scripts/monitor.js
+++ forge-ffi-scripts/monitor.js
@@ -24,16 +24,17 @@ async function commit(provider,lottery) {
   const minBets = process.env.MIN_BETS ? parseInt(process.env.MIN_BETS) : 8;
   const minBlocks = process.env.MIN_BLOCKS ? parseInt(process.env.MIN_BLOCKS) : 30*60; // 60 minutes on Base
   const maxUpdate = process.env.MAX_UPDATE ? parseInt(process.env.MAX_UPDATE) : 179;
-  const minBetSum = process.env.MIN_BET_SUM ? parseInt(process.env.MIN_BET_SUM) : 1000000000; // power:10
+  const minBetSum = process.env.MIN_BET_SUM ? ethers.utils.parseUnits(process.env.MIN_BET_SUM, 18) : ethers.BigNumber.from(1000000000); // power:10
   const blockNumber = await provider.getBlockNumber();
   const nextIndex = await lottery.nextIndex();
   const betsIndex = await lottery.betsIndex();
   const commitIndex = await lottery.commitIndex();
   const betSum = await lottery.betSum();
+  const betSum_in_FOOM = ethers.utils.parseUnits(betSum.toString(), 18);
   const [lastIndex,lastBlockNumber,lastRoot,lastLeaf] = readLast();
   if(betsIndex > 0 && lastIndex == nextIndex && commitIndex == 0) {
     const waitingBlocknumber = readWaitingBlocknumber();
-    if((waitingBlocknumber > 0 && waitingBlocknumber <= blockNumber - minBlocks) || (betsIndex >= minBets) || (betSum >= minBetSum)) {
+    if((waitingBlocknumber > 0 && waitingBlocknumber <= blockNumber - minBlocks) || (betsIndex >= minBets) || (betSum_in_FOOM.gt(minBetSum))) {
       // commit if betsIndex is not 0 and enough time has passed
       const revealSecretInput = process.env.PRIVATE_KEY+'_FOOM_'+nextIndex.toString();
       console.log(revealSecretInput,"reveal secret input");
